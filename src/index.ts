92ff6dbf3846bef8052a219bb9ec3b2b15314a4e
Zaahiid <zahidrsheikh01@gmail.com>
2024-05-15 00:01:25 +0530

    Database Setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-import express,{Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("MongoDB Connected! 😍");
+  })
+  .catch((err) => {
+    console.log(`Error from DB connection`, err);
+  });
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get("/api/test", async(req:Request,res:Response)=>{
-    res.json({message:"Hello from Express endpoint!"})
-})
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello from Express endpoint!" });
+});
 
-app.listen(7000,()=>{
-    console.log(`Server is running on Port 7000`)
-})
+app.listen(7000, () => {
+  console.log(`Server is running on Port 7000`);
+});
