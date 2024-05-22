fe96d5094dc036c65a3d4a293fa8d34cb01f0043
Zaahiid <zahidrsheikh01@gmail.com>
2024-05-22 23:28:32 +0530

    Added Login functionality 🤝

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users"
+import authRoutes from "./routes/auth"
 
 mongoose
   .connect(process.env.MONGO_URI as string)
@@ -22,6 +23,7 @@ app.use(cors());
 //   res.json({ message: "Hello from Express endpoint!" });
 // });
 
+app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 
 app.listen(7000, () => {
