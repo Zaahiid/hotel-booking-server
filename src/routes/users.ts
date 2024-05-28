a6323bd7b6815812996924b7ab1c022bd6029e4e
Zaahiid <zahidrsheikh01@gmail.com>
2024-05-28 23:54:47 +0530

    Resolved the syntax error from Client - "OK is not json"

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -40,7 +40,8 @@ router.post(
         secure: process.env.NODE_ENV === "production",
         maxAge: 1000 * 60 * 60 * 24,
       });
-      return res.sendStatus(200);
+      return res.status(200).send({
+        message: "User registered OK",})
     } catch (error) {
       console.log(error);
       res.status(500).send({ message: "Something went wrong" });
